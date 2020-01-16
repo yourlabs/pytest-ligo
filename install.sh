601c763d79b27cf8e28cb9a6efc336a8df9ff6f0
jpic <jpic@yourlabs.org>
2020-01-16 09:37:12 +0000

    Refactor tightly

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash -eux
 URL='https://gitlab.com/ligolang/ligo/-/jobs/artifacts/dev/download?job=build-and-package-ubuntu-18-04'
-rm -f 'download?job=build-and-package-ubuntu-18-04'
 wget $URL
 unzip -u 'download?job=build-and-package-ubuntu-18-04'
 ar xv dist/package/ubuntu-18.04/*.deb
 tar xvf data.tar.xz
+rm -rf *.tar.xz dist debian-binary 'download?job=build-and-package-ubuntu-18-04*'
 mkdir -p ~/.local/bin
 cp bin/ligo ~/.local/bin
 touch ~/.bash_profile
