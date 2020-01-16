93baa8953ff759014e726bef2161439f7b42dac3
jpic <jpic@yourlabs.org>
2020-01-16 08:52:06 +0000

    Let ligo install outside

--- pytest_ligo.py
+++ pytest_ligo.py
@@ -29,31 +29,14 @@ class Contract:
 class Ligo:
     @property
     def bin(self):
-        loc = Path(os.path.dirname(__file__)) / 'bin' / 'ligo'
-        if loc.exists():
-            return loc
+        own = Path(os.getenv('HOME')) / '.local' / 'bin' / 'ligo'
+        if own.exists():
+            return str(own)
 
         try:
             return subprocess.check_output('which ligo', shell=True).decode('utf8').strip()
         except subprocess.CalledProcessError:
-            response = requests.get(URL)
-            zipfile = ZipFile(io.BytesIO(response.content))
-            old_cwd = os.getcwd()
-            os.chdir(os.path.dirname(__file__))
-            zipfile.extractall()
-            subprocess.check_call('''
-                ar xv dist/package/ubuntu-18.04/*.deb
-                tar xvf data.tar.xz
-                mkdir -p ~/.local/bin
-                cp bin/ligo ~/.local/bin
-                if test -f ~/.bash_profile; then
-                  if ! grep local/bin ~/.bash_profile; then
-                    echo 'export PATH="$HOME/.local/bin:$PATH"' > ~/.bash_profile
-                  fi
-                fi
-            ''', shell=True)
-            os.chdir(old_cwd)
-        return loc
+            print('ligo not available, try install.sh')
 
     def compile(self, arg):
         if isinstance(arg, AbstractTestType):
