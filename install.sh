e6881814ae7f389841173a7c8b9b762d1a7f2c5c
jpic <jpic@yourlabs.org>
2020-01-16 10:00:29 +0000

    Improve ligo installer

--- install.sh
+++ install.sh
@@ -7,8 +7,12 @@ ar xv dist/package/ubuntu-18.04/*.deb
 tar xvf data.tar.xz
 rm -rf *.tar.xz dist debian-binary 'download?job=build-and-package-ubuntu-18-04*'
 mkdir -p ~/.local/bin
-cp bin/ligo ~/.local/bin
+mv bin/ligo ~/.local/bin
+ln -sfn ~/.local/bin/ligo bin/ligo
 touch ~/.bash_profile
 if ! grep local/bin ~/.bash_profile; then
-  echo 'export PATH="$HOME/.local/bin:$PATH"' > ~/.bash_profile
+    echo 'export PATH="$HOME/.local/bin:$PATH"' > ~/.bash_profile
+fi
+if [ "$USER" = "root" ]; then
+    ln -sfn ~/.local/bin/ligo /bin/ligo
 fi
